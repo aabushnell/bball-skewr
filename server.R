993671db413e8c521690479e4e8079411e29a292
Aaron Bushnell <aabushnell@gmail.com>
2022-08-22 07:20:16 +0200

    Renamed functions for consistency

--- server.R
+++ server.R
@@ -141,7 +141,7 @@ shinyServer(function(input, output, session){
     if (use_default_shots) {
       default_shots
     } else {
-      fetch_shots_by_player_id_and_season(
+      get_shots_by_player_id_and_season(
         current_player()$person_id,
         current_season(),
         current_season_type()
